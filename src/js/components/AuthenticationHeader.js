44f0dca8d012dcf025f576e0b25e97f524f8db68
Jagat Jeevan Sahoo <kanhajeevan@gmail.com>
2018-07-12 09:07:12 +0530

    Change logo icon

--- src/js/components/AuthenticationHeader.js
+++ src/js/components/AuthenticationHeader.js
@@ -4,10 +4,7 @@ import { labels } from '../constants';
 const AuthenticationHeader = () => (
   <section className="logo-container">
     {labels.bannerText}
-    <i className="icon-home_logo">
-      <span className="path1" />
-      <span className="path2" />
-    </i>
+    <i className="icon-home_logo" />
   </section>
 );
 
