17499dd15f986ef448cfb5cb745b3923e830c8ee
Jagat Jeevan Sahoo <jagatsah@thoughtworks.com>
2019-10-08 16:09:53 +0530

    test commit

--- src/js/reducers/signUp.js
+++ src/js/reducers/signUp.js
@@ -6,14 +6,16 @@ const initialState = {
   fieldValue: '',
   status: '',
   message: '',
+  somefield: {},
 };
 
 export default function signUp(state = initialState, action) {
   switch (action.type) {
     case actionTypes.SIGNUPSUCCESS:
-      return { ...state, ...action.payload };
+      return { ...state, somefield: action.payload };
+
     case actionTypes.SIGNUPRESET:
     default:
-      return { ...state, ...initialState };
+      return state;
   }
 }
