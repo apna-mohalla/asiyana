5d734eb319435753642014e515db18cab0d488fd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-09 12:20:34 +0000

    Deploy apna-mohalla/asiyana to github.com/apna-mohalla/asiyana.git:gh-pages

--- src/js/views/ForgotPassword/ForgotPasswordComponent.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const ForgotPasswordComponent = () => <div>This is Forgot password component</div>;
-
-export default ForgotPasswordComponent;
--- src/js/views/SignUp/SignUpComponent.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const SignUpComponent = () => <div>This is Sign up component</div>;
-
-export default SignUpComponent;
