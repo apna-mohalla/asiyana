202fc4d6e95eb0e6c3094071cde08bca55ec7d09
Jagat Jeevan Sahoo <Jeevan_Sahoo_Jagat@external.mckinsey.com>
2019-10-09 16:53:47 +0530

    Remove deploy scripts

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-git checkout -B gh-pages
-git add -f build -n
-git commit -am "Deployed website" -n
-git filter-branch -f --prune-empty --subdirectory-filter build
-git push -f origin gh-pages -n
-git checkout -
