21fa91177f264a770ae3200f661fd8a8c8e77d90
Jagat Jeevan Sahoo <jagatsah@thoughtworks.com>
2019-10-13 23:44:55 +0530

    Missed import

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,4 +1,4 @@
-const version = 3;
+const version = 4;
 // Use this function if we require to add functionality.
 // self.addEventListener('notificationclose', (e) => {
 //   // const { notification } = e;
--- src/js/views/SignUp/SignUpComponent.js
+++ src/js/views/SignUp/SignUpComponent.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
